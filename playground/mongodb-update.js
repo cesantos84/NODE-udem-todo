5f80171e78487bcd7a7a8db17acbf865dbf698f0
cesantos84 <chris.santos@milestonetech.com>
2017-07-19 13:34:49 -0600

    add upd

---
+++ playground/mongodb-update.js
@@ -0,0 +1,27 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+
+	//findOneAndUpdate
+	db.collection('Todos').findOneAndUpdate({
+		_id: new ObjectID('596faf7b5d7559b41d933659')
+	}, {
+		$set: {
+			completed: false
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	//db.close();
+});
