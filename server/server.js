723971fd9ba0721bc76f50f5e85b34bcdd1ca0d5
cesantos84 <chris.santos@milestonetech.com>
2017-08-21 13:38:04 -0600

    catchup/GET users/me and auth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 	//cd C:\Program Files\MongoDB\Server\3.4\bin
 	//mongod.exe --dbpath /web/Mongo-data
 /*db startup*/
+require('./config/config');
+
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -9,9 +12,10 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -90,11 +94,57 @@ app.delete('/todos/:id', (req, res) => {
 			//if no todo - send 404 w/ empty body
 		//error
 			//400 - send back empty
-	Todo.find().then((todos) => {
+	/*Todo.find().then((todos) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
-	})
+	})*/
+});
+
+app.patch('/todos/:id', (res, req) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completed = new Date().getTime();
+	} else { 
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+		if(!todo){
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+app.patch('/users', (res, req) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.findByToken;
+	user.generateAuthToken;
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth').send(user);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
